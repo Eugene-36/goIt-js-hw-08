6340a9437cd21d5e90b4cc4922db1a7372b6615c
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2020-11-22 01:16:59 +0200

    Добавляет js-файл

---
+++ js/gallery-items.js
@@ -0,0 +1,129 @@
+const photos = [
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+      description: 'Hokkaido Flower',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+      description: 'Container Haulage Freight',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+      description: 'Aerial Beach View',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+      description: 'Flower Blooms',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+      description: 'Alpine Mountains',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+      description: 'Mountain Lake Sailing',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+      description: 'Alpine Spring Meadows',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+      description: 'Nature Landscape',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+      description: 'Lighthouse Coast Sea',
+    },
+  ];
+
+const ulContainer = document.querySelector('.gallery')
+console.log(ulContainer)
+
+const cardsMarkUp = creatGalleryMarkUp(photos)
+// const cardsMarkUpTwo = onClickMarkUp(photos)
+
+ulContainer.insertAdjacentHTML('beforeend', cardsMarkUp)
+ulContainer.addEventListener('click', onClickMarkUp)
+
+
+function creatGalleryMarkUp(photos) {
+  return photos.map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${original}"
+    >
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+  }).join('')
+
+}
+
+
+// // Первое сделать так , чтобы при клике на кртинку открывалась модалка
+
+
+
+function onClickMarkUp() {
+
+const newClass = document.querySelector('.js-lightbox')
+newClass.classList.add('is-open')
+
+
+  // console.log(newClass)
+  //  if(evt.target.nodeName !== 'IMG'){
+  //    return
+  //  }
+ const clean = document.getElementsByClassName('.lightbox__image')
+ clean.setAttribute('src', "");
+
+
+};
+
+// const img = evt.target;
+// const tag = img.dataset.value;
+// const isOpen = img.classList.contains('is-open');
+
+// if (isOpen) {
+//     selectedTags.delete(tag);
+// } else {
+//     selectedTags.add(tag);
+// }
+
+// img.classList.toggle('is-open');
+// console.log(onClickMarkUp);
+
